f98ec1aaafec1b2453f101ccc0924e790f668aa0
Tim Trommel <tim.trommel@live.nl>
2022-06-14 17:17:27 +0200

    End of section 2

---
+++ Persistence/Migrations/20220614134206_InitialCreate2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Activities");
+        }
+    }
+}
